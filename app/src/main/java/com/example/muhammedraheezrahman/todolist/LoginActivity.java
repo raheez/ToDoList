62401af125b99de7aeb0bd0e91538593aac0aa11
Muhammed raheez Rahman <muhammedraheezrahman@Muhammeds-MacBook-Pro.local>
2018-12-24 16:18:51 +0400

    added firebase login

---
+++ app/src/main/java/com/example/muhammedraheezrahman/todolist/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.muhammedraheezrahman.todolist;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText emailEt,passEt;
+    Button login,signup;
+    String email,password;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = (Button) findViewById(R.id.loginBut);
+        signup = (Button) findViewById(R.id.signupBut);
+        emailEt = (TextInputEditText) findViewById(R.id.email_et);
+        passEt = (TextInputEditText) findViewById(R.id.passEt);
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser()!=null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            finish();
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = emailEt.getText().toString();
+                password = passEt.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()){
+
+                    auth.signInWithEmailAndPassword(email,password).
+                            addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if(!task.isSuccessful()){
+
+                                        Log.d("RanLogin","Login failed");
+                                        Toast.makeText(getApplicationContext(),"Not successfull",Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Log.d("RanLogin","Login success");
+                                        Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+
+
+            }
+        });
+    }
+}
